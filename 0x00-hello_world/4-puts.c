79ad7a8a6f3c1681cd89c9a3eda3062ca03d5b91
Alazar-dev <mealazartadesse@gmail.com>
2021-08-26 16:34:10 +0300

    removed whitespace

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,8 +3,8 @@
  * main - prints a text to standard output
  * Return: returns 0 since it is void
  */
-int main(void) 
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return (0);	
+	return (0);
 }
