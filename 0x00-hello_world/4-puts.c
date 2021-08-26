b4021e995bdae7a6172077e34d059016a4f524ce
Alazar-dev <mealazartadesse@gmail.com>
2021-08-26 16:30:54 +0300

    betty

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,8 @@
 #include <stdio.h>
+/**
+ * main - prints a text to standard output
+ * Return: returns 0 since it is void
+ */
 int main(void) 
 {
 	puts("\"Programming is like building a multilingual puzzle");
